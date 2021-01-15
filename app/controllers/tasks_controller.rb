788237719fcb2656f64e5f375ae7ecc460a69062
nahoom <nahoom.academy@gmail.com>
2021-01-15 11:25:19 +0000

    kadai_fb_1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,11 +46,10 @@ class TasksController < ApplicationController
     flash[:success] = 'タスク は正常に削除されました'
     redirect_to tasks_url
   end
-end
 
-private
-
-# Strong Parameter
+  private
+  # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
   end
+end
