5ea098738f36d275eccfd1ca7c7a1a8785880987
nahoom <nahoom.academy@gmail.com>
2021-01-31 13:43:15 +0000

    add login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,10 @@ class TasksController < ApplicationController
   before_action :set_tasks, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
